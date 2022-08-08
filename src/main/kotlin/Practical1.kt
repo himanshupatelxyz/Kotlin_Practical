011dcbdfcb56513cbe43eefc9159a70dbdd79fd3
himanshupatelxyz <himanshupatelxyz@gmail.com>
2022-08-08 12:03:05 +0530

    UptoDatatype conversion

--- src/main/kotlin/Practical1.kt
+++ src/main/kotlin/Practical1.kt
@@ -1,6 +1,21 @@
 fun main(){
-
-    print("Hello world");
-    println("I am a good coder")
-    print("Belive in your self")
+    // DataType example in Kotlin
+    var a:Int=22
+    var b:Float=1.5f
+    var c:Char='T'
+    var str:String="Hello class"
+    var bool:Boolean=false
+    var double:Double=96.36
+    var long:Long=338945678
+    var short:Short=-2
+    var byte:Byte=127
+    println("Interger Value:"+a)
+    println("Float value:"+b)
+    println("Charaacter value:$c")
+    println("String value:${str.length}")
+    println("Boolean value:"+bool)
+    println("Double value:"+double)
+    println("long value:"+long)
+    println("Short value:"+short)
+    println("Byte value:$byte")
 }
--- src/main/kotlin/Practical2.kt
+++ src/main/kotlin/Practical2.kt
@@ -1,4 +1,24 @@
-fun main(){
+import java.util.StringJoiner
 
-   print("Hello wolrd")
+fun main(){
+   println("Type conversion")
+   var a:Int=10
+   var int:Int=20
+   println("Integer value:"+a)
+   var b:Double
+   b=a.toDouble()
+   println("Double value (From Integer):"+b)
+   var c:String="10"
+   var str:String="A"
+   var strtoint:Int?=c.toIntOrNull()
+   var strtoint1:Int?=str.toIntOrNull()
+//   var strtoint1:Int=str.toInt()!!
+   //var strtoint1:Int=str.toInt()
+   var float:Float=int.toFloat()
+   println("Int to Float:"+float)
+   var f:Float=30.40f
+   var floattoint:Int=f.toInt()
+   println("float to Int is:"+floattoint)
+   println("Integer value1 from string:"+strtoint)
+  println("Integer value2 from string:"+strtoint1)
 }
